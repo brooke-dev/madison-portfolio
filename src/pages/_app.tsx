d08fdc9b7b25b0016eac58a9a96d6aab0ed00dda
Brooke Ross <brooke.ross2800@gmail.com>
2023-04-19 10:54:46 -0600

    NavBar component and tailwind CSS added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '@/styles/globals.css'
+// import type { AppProps } from 'next/app'
+
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
+
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import Navbar from '../components/NavBar';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  return (
+    <div>
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
+  );
 }
