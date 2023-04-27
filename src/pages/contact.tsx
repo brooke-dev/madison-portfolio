c2aa806985409550f8091d73a23cb851caef9f93
Chris Mann <chrispmann@gmail.com>
2023-04-26 20:33:19 -0700

    improved formatting of form; submit functionality disabled for now

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useRouter } from "next/router";
-import { sendEmail } from "../api/sendgrid";
+// import { sendEmail } from "sendemail";
 
 const ContactForm = () => {
     const [name, setName] = useState("");
@@ -17,31 +17,36 @@ const ContactForm = () => {
     }
 
     // Send the email
-    await sendEmail({
-        to: process.env.CONTACT_EMAIL,
-        from: email,
-        subject: `Contact Form Submission from ${name}`,
-        text: message,
-    });
+    // await sendEmail({
+    //     to: process.env.CONTACT_EMAIL,
+    //     from: email,
+    //     subject: `Contact Form Submission from ${name}`,
+    //     text: message,
+    // });
 
     // Redirect to the success page
     router.push("/contact/success");
     };
 
     return (
+    <div className="flex flex-col items-center justify-center h-screen mt-11">
+    <h1 className="text-4xl">Contact Form</h1>
+    <h3 className="mt-4">Sign up for email updates</h3>
+    <div className="flex flex-wrap justify-center mt-4"></div>
+    
     <form onSubmit={handleSubmit}>
         <input
         type="text"
         placeholder="Name"
         value={name}
         onChange={(e) => setName(e.target.value)}
-        />
+        /><br />
         <input
         type="email"
         placeholder="Email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
-        />
+        /><br/>
         <textarea
         placeholder="Message"
         value={message}
@@ -49,6 +54,7 @@ const ContactForm = () => {
         />
         <button type="submit">Submit</button>
     </form>
+    </div>
     );
 };
 
