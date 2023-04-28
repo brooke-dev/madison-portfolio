7e66ce9ac1c80336a3f0cb004cc91b3e97e769af
Chris Mann <chrispmann@gmail.com>
2023-04-28 09:52:57 -0700

    improved contact page, uncommented event handling but no API key yet

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useRouter } from "next/router";
-// import { sendEmail } from "sendemail";
+import { sendEmail } from "sendemail";
 
 const ContactForm = () => {
     const [name, setName] = useState("");
@@ -17,44 +17,49 @@ const ContactForm = () => {
     }
 
     // Send the email
-    // await sendEmail({
-    //     to: process.env.CONTACT_EMAIL,
-    //     from: email,
-    //     subject: `Contact Form Submission from ${name}`,
-    //     text: message,
-    // });
+    await sendEmail({
+        to: process.env.CONTACT_EMAIL,
+        from: email,
+        subject: `Contact Form Submission from ${name}`,
+        text: message,
+    });
 
     // Redirect to the success page
     router.push("/contact/success");
     };
 
     return (
-    <div className="flex flex-col items-center justify-center h-screen mt-11">
-    <h1 className="text-4xl">Contact Form</h1>
-    <h3 className="mt-4">Sign up for email updates</h3>
-    <div className="flex flex-wrap justify-center mt-4"></div>
-    
-    <form onSubmit={handleSubmit}>
-        <input
-        type="text"
-        placeholder="Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        /><br />
-        <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        /><br/>
-        <textarea
-        placeholder="Message"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        />
-        <button type="submit">Submit</button>
-    </form>
+
+    <section className="bg: black">
+        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
+            <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Let us know!</p>
+            <form action="#" className="space-y-8">
+                <div>
+                    <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your name</label>
+                    <input 
+                    type="email" 
+                    id="email" 
+                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
+                    placeholder="namey name name" required/>
+                </div>
+                <div>
+                    <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+                    <input 
+                    type="email" 
+                    id="email" 
+                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
+                    placeholder="me@somewhere.com" required/>
+                </div>
+                <div className="sm:col-span-2">
+                    <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+                    <textarea id="message" 
+                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
+            </div>
+            <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+        </form>
     </div>
+</section>
     );
 };
 
